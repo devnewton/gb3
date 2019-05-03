066203cd145662a42b38d596bdd101615b355e04
devnewton <devnewton@bci.im>
2019-05-03 11:41:29 +0200

    add select and input

--- coincoin/js/coincoin.js
+++ coincoin/js/coincoin.js
@@ -2,18 +2,53 @@ class Gb3Coincoin extends HTMLElement {
 
     constructor() {
         super();
+        this.tribunes = new Set();
     }
 
     connectedCallback() {
-        let postsElement = document.createElement('gb3-posts');
-        this.appendChild(postsElement);
+        this.setupTribuneSelect();
+        this.setupMessageInput();
+        this.setupPostsElement();
+        this.startPoll();
+    }
 
-        var postSource = new EventSource("/api/poll");
-        postSource.onmessage = function (event) {
-            console.log(event.data);
+    setupTribuneSelect() {
+        this.tribuneSelect = document.createElement("select");
+        let viewAllTribuneOption = document.createElement("option");
+        viewAllTribuneOption.value = "*";
+        viewAllTribuneOption.text = "All";
+        this.tribuneSelect.add(viewAllTribuneOption);
+        this.appendChild(this.tribuneSelect);
+    }
+
+    setupMessageInput() {
+        this.messageInput = document.createElement("input");
+        this.messageInput.type = "text";
+        this.messageInput.spellcheck = true;
+        this.appendChild(this.messageInput); 
+    }
+
+    setupPostsElement() {
+        this.postsElement = document.createElement('gb3-posts');
+        this.appendChild(this.postsElement);
+    }
+
+    startPoll() {
+        let postSource = new EventSource("/api/poll");
+        postSource.onmessage = (event) => {
             let post = JSON.parse(event.data);
-            postsElement.insertPost(post);
+            this.postsElement.insertPost(post);
+            this.addTribune(post.tribune);
         };
+    }
+
+    addTribune(tribune) {
+        if(!this.tribunes.has(tribune)) {
+            this.tribunes.add(tribune);
+            var option = document.createElement("option");
+            option.text = option.value = tribune;
+            this.tribuneSelect.add(option);
+        }
     }
 
 }
