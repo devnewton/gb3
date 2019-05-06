f137824eb45f4b5989193ac3c1f5f55dd05d9769
devnewton <devnewton@bci.im>
2019-05-06 18:54:11 +0200

    tsv backend

--- gb0/main.go
+++ gb0/main.go
@@ -1,24 +1,67 @@
-package main
+package gb0
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 var listenAddress string
 var verboseMode bool
+var storeFile string
 
 func init() {
 	flag.StringVar(&listenAddress, "listen", ":6667", "TCP address to listen on")
 	flag.BoolVar(&verboseMode, "verbose", false, "Verbose logging")
+	flag.StringVar(&storeFile, "file", "/tmp/gb0.json", "File to store posts")
 }
+
+type post struct {
+	ID      int64  `json:"id"`
+	Time    string `json:"time"`
+	Info    string `json:"info"`
+	Message string `json:"message"`
+}
+
+type posts []post
+
+func (p posts) Len() int           { return len(p) }
+func (p posts) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p posts) Less(i, j int) bool { return p[i].ID < p[j].ID }
 
 type gb0 struct {
 }
 
 func newGb0() *gb0 {
 	return &gb0{}
+}
+
+func readPosts() (p posts) {
+	jsonFile, err := os.Open(storeFile)
+	if nil != err {
+		log.Println(err)
+		return p
+	}
+	defer jsonFile.Close()
+	decoder := json.NewDecoder(jsonFile)
+	err = decoder.Decode(&p)
+	if nil != err {
+		return p
+	}
+	return p
+}
+
+func stripControlsCharsFromString(str string) string {
+	return strings.Map(func(r rune) rune {
+		if r >= 32 && r != 127 {
+			return r
+		}
+		return -1
+	}, str)
 }
 
 func (g *gb0) handlePost(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +75,9 @@ func (g *gb0) handlePost(w http.ResponseWriter, r *http.Request) {
 
 func (g *gb0) handleGetTsv(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/tab-separated-values")
-	//TODO
+	for _, p := range readPosts() {
+		fmt.Fprintf(w, "%d\t%s\t%s\t%s\t\n", p.ID, p.Time, p.Info, p.Message)
+	}
 }
 
 func main() {
